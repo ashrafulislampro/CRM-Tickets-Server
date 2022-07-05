df79b6a48883709b6cd2f128e0927921fcbb63f9
ashrafulislampro <ashrafulislam1659@gmail.com>
2022-07-05 09:15:56 +0600

    user authentication with jwt

---
+++ src/helpers/jwt.helper.js
@@ -0,0 +1,27 @@
+var jwt = require("jsonwebtoken");
+const { setJWT, getJWT } = require("./redis.helper");
+
+const createAccessJWT = async (email, _id) => {
+  try {
+    const accessJWT = await jwt.sign({ email }, process.env.JWT_ACCESS_SECRET, {
+      expiresIn: "15m",
+    });
+
+    setJWT(accessJWT, _id);
+    return Promise.resolve(accessJWT);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+const createRefreshJWT = (payload) => {
+  const refreshJWT = jwt.sign({ payload }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: "30d",
+  }); // process.env.JWT_ACCESS_SECRET in video
+  return Promise.resolve(refreshJWT);
+};
+
+module.exports = {
+  createAccessJWT,
+  createRefreshJWT,
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { insertUser, getUserByEmail } = require("../model/user/User.model");
 const { hashPassword, comparePassword } = require("../helpers/bcrypt.helper");
+const { createAccessJWT, createRefreshJWT } = require("../helpers/jwt.helper");
 
 router.all("/", (req, res, next) => {
   // res.json({ message: "return form user router" });
@@ -44,18 +45,29 @@ router.post("/login", async (req, res) => {
 
   // get user with email from db
   const user = await getUserByEmail(email);
-  // console.log(user);
 
   // hash our password and compaire with the db one
   const passFromDB = user && user._id ? user.password : null;
 
-  if (!passFromDB)
+  if (!passFromDB) {
     return res.json({ status: "error", message: "Invalid email or password!" });
+  }
 
   const result = await comparePassword(password, passFromDB);
-  console.log(result);
 
-  res.json({ status: "Success", message: "Login Successfully!" });
+  if (!result) {
+    return res.json({ status: "error", message: "Invalid email or password!" });
+  }
+
+  const accessJWT = await createAccessJWT(user.email, `${user._id}`);
+  const refreshJWT = await createRefreshJWT(user.email);
+
+  res.json({
+    status: "Success",
+    message: "Login Successfully!",
+    accessJWT,
+    refreshJWT,
+  });
 });
 
 module.exports = router;
